dabbf9a85409dd12c7f18f977130ed3fc27c4d89
maehner <maehner@kontrollfeld>
2016-08-07 11:17:19 +0200

    Menu Bugfix

--- js/main.js
+++ js/main.js
@@ -14,4 +14,31 @@ $(document).ready(function(){
     });
   });
 
+
+  //ONLOAD FADE
+  $('body').css('opacity','1');
+
+
+  //ANIMATED SCROLL
+  $('a[href*="#"].js-aniscroll:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 300);
+        return false;
+      }
+    }
+
+  });
+
+  //on click close menu
+  $('.menu-item').click(function(){
+    $('.menu-open').prop('checked', false);
+  });
+
+
+
 });/*END SCRIPT*/
